e6471dda774ba67b9617d281c46e4f78de8403bb
Guillaume BÉNIT <g.benit@laposte.net>
2016-12-23 21:32:40 +0100

    House Number

---
+++ lua/entities/guigui_door/shared.lua
@@ -0,0 +1,7 @@
+ENT.Base 			= "base_nextbot"
+ENT.Spawnable		= true
+ENT.AdminOnly		= true
+ENT.PrintName		= "Houses register"
+ENT.Category		= "Houses register"
+ENT.Author			= "Guillaume"
+ENT.Contact			= "http://steamcommunity.com/id/guillaume_/"
