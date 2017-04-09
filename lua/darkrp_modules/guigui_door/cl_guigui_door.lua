d5431414ca2cc2f7a48ce55f139956c77c3e7a98
PichotM <pichotcontact@gmail.com>
2017-04-09 21:47:40 +0200

    Update cl_guigui_door.lua

--- lua/darkrp_modules/guigui_door/cl_guigui_door.lua
+++ lua/darkrp_modules/guigui_door/cl_guigui_door.lua
@@ -1,36 +1,39 @@
+
 surface.CreateFont("guigui_door_font",{
-	font="DermaLarge",
-	size=25,
-	antialias=false,
+    font="DermaLarge",
+    size=25,
+    antialias=false,
 })
-function guigui_door(door)
-	if door:GetClass()!="prop_door_rotating" then return end
-	if door:isKeysOwned() then
-		local ang=door:GetAngles()
-		local GetID=string.Explode(",",string.Comma(door:getDoorOwner():SteamID64()),false)
-		local ID=GetID[6]..GetID[1]
-		cam.Start3D()
-			ang:RotateAroundAxis(ang:Right(),-90)
-			ang:RotateAroundAxis(ang:Up(),90)
-			cam.Start3D2D(door:LocalToWorld(Vector(0,3.5,50)),ang,0.1)
-				surface.SetMaterial(Material("guigui/door/plate.png","unlitgeneric smooth"))
-				surface.SetDrawColor(255,255,255,255)
-				surface.DrawTexturedRect(0,0,65,35)
-				surface.SetTextPos(5,5)
-				surface.SetFont("guigui_door_font") 
-				surface.DrawText(ID)
-			cam.End3D2D()
-			ang:RotateAroundAxis(ang:Right(),180)
-			cam.Start3D2D(door:LocalToWorld(Vector(0,3.5+6.75,50)),ang, 0.1 )
-				surface.SetMaterial(Material("guigui/door/plate.png","unlitgeneric smooth"))
-				surface.SetDrawColor(255,255,255,255)
-				surface.DrawTexturedRect(0,0,65,35)
-				surface.SetTextPos(5,5)
-				surface.SetFont("guigui_door_font") 
-				surface.DrawText(ID)
-			cam.End3D2D()
-		cam.End3D()
-		return true
-	end
+
+local matDoor = Material("guigui/door/plate.png","unlitgeneric smooth")
+
+local function guigui_door(door)
+    if door:isKeysOwned() then
+        local ang = door:GetAngles()
+        local GetID = string.Explode(",",string.Comma(door:getDoorOwner():SteamID64()),false)
+        local ID= GetID[6].. GetID[1]
+        cam.Start3D()
+            ang:RotateAroundAxis(ang:Right(),-90)
+            ang:RotateAroundAxis(ang:Up(),90)
+            cam.Start3D2D(door:LocalToWorld(Vector(0,3.5,50)),ang,0.1)
+                surface.SetMaterial(matDoor)
+                surface.SetDrawColor(color_white)
+                surface.DrawTexturedRect(0,0,65,35)
+                surface.SetTextPos(5,5)
+                surface.SetFont("guigui_door_font") 
+                surface.DrawText(ID)
+            cam.End3D2D()
+            ang:RotateAroundAxis(ang:Right(),180)
+            cam.Start3D2D(door:LocalToWorld(Vector(0,3.5+6.75,50)),ang, 0.1 )
+                surface.SetMaterial(matDoor)
+                surface.SetDrawColor(color_white)
+                surface.DrawTexturedRect(0,0,65,35)
+                surface.SetTextPos(5,5)
+                surface.SetFont("guigui_door_font") 
+                surface.DrawText(ID)
+            cam.End3D2D()
+        cam.End3D()
+        return true
+    end
 end
-hook.Add("HUDDrawDoorData","guigui_door",guigui_door)
+hook.Add("HUDDrawDoorData","guigui_door", guigui_door)
