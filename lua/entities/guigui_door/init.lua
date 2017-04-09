9ae0551e0064c0b179b75a8ff865173e98c0e30e
PichotM <pichotcontact@gmail.com>
2017-04-09 21:10:05 +0200

    Update init.lua

--- lua/entities/guigui_door/init.lua
+++ lua/entities/guigui_door/init.lua
@@ -4,21 +4,16 @@ AddCSLuaFile("cl_init.lua")
 include("shared.lua")
 
 function ENT:Initialize()
-	self:SetUseType(SIMPLE_USE)
-	self:SetModel("models/Humans/Group01/Male_01.mdl")
-	self.IdleSequence=5
-	self:SetHealth(2147483647)
+	self:SetModel("models/Humans/Group01/Male_01.mdl")	
+	self:SetHullType( HULL_HUMAN )
+	self:SetHullSizeNormal( )
+	self:SetSolid( SOLID_BBOX )
+	self:SetUseType( SIMPLE_USE )
+	self:DropToFloor()
 end
 
 function ENT:Use(ply)
-	if IsValid(ply) and ply:IsPlayer() then
+	if IsValid(ply) and ply:IsPlayer() and ply:GetPos():Distance(self:GetPos()) <= 200 and cal:GetEyeTraceNoCursor().Entity == self then
 		ply:SendLua('Entity('..self:EntIndex()..'):Use()')
-	end
-end
-
-function ENT:RunBehaviour()
-	while true do
-		self:PlaySequenceAndWait(self.IdleSequence)
-		coroutine.yield()
 	end
 end
